24af96181e9844133db133e4808f3b2ca7af7db9
Aleksey <aleksey_s@sibmail.com>
2022-03-05 12:42:58 +0700

    add Lab 2

---
+++ Lab_2.2/Solution/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Solution.Models;
+using System.Diagnostics;
+
+namespace Solution.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult ParsingInSingleAction()
+        {
+            if (Request.Method == "POST")
+            {
+                CulcData culcData = new CulcData
+                {
+                    a = Convert.ToInt32(Request.Form["a"]),
+                    b = Convert.ToInt32(Request.Form["b"]),
+                    operation = (Operations)Enum.Parse(typeof(Operations), Request.Form["operation"])
+                };
+
+                ViewBag.Result = culcData.Culc();
+
+                return View("Result");
+            } 
+            return View("CulcForm");
+        }
+
+        [HttpGet]
+        [ActionName("ManualParsingSeparate")]
+        public IActionResult ParsingInSeparateActionsGet()
+        {
+            return View("CulcForm");
+        }
+
+        [HttpPost]
+        [ActionName("ManualParsingSeparate")]
+        public IActionResult ParsingInSeparateActionsPost()
+        {
+            CulcData culcData = new CulcData
+            {
+                a = Convert.ToInt32(Request.Form["a"]),
+                b = Convert.ToInt32(Request.Form["b"]),
+                operation = (Operations)Enum.Parse(typeof(Operations), Request.Form["operation"])
+            };
+
+            ViewBag.Result = culcData.Culc();
+
+            return View("Result");
+        }
+
+        [HttpGet]
+        public IActionResult ModelBindingParameters()
+        {
+            return View("CulcForm");
+        }
+
+        [HttpPost]
+        public IActionResult ModelBindingParameters(int a, int b, Operations operation)
+        {
+            CulcData culcdata = new CulcData
+            {
+                a = a,
+                b = b,
+                operation = operation
+            };
+
+            ViewBag.Result = culcdata.Culc();
+
+            return View("Result");
+        }
+
+        [HttpGet]
+        public IActionResult ModelBindingSeparate()
+        {
+            return View("CulcForm");
+        }
+
+        [HttpPost]
+        public IActionResult ModelBindingSeparate(CulcData culcdata)
+        {
+            ViewBag.Result = culcdata.Culc();
+
+            return View("Result");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab_2.2/Solution/Models/CulcData.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Solution.Models
+{
+    public enum Operations
+    {
+        [Display(Name="+")]
+        Plus,
+        [Display(Name = "-")]
+        Minus,
+        [Display(Name = "*")]
+        Multiply,
+        [Display(Name = "/")]
+        Divide,
+
+    }
+
+    public class CulcData
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public Operations operation { get; set; }
+
+        public int Culc()
+        {
+            return operation switch
+            {
+                Operations.Plus => a + b,
+                Operations.Minus => a - b,
+                Operations.Multiply => a * b,
+                Operations.Divide => a / b,
+            };
+        }
+    }
+}
