a6a591fb0916861c85c97fcd6156d04871370b4c
Aleksey <aleksey_s@sibmail.com>
2022-03-07 15:01:26 +0700

    add Lab 3

---
+++ Lab_2.3/Solution/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Solution.Models;
+using System.Diagnostics;
+
+namespace Solution.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            CulcData culcData = CulcData.Instance;
+            
+
+            return View(culcData);
+        }
+
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            CulcData culcData = CulcData.Instance;
+            culcData.Reset();
+            culcData.CreateQuestion();
+
+            return View(culcData);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(CulcData culcData, string Action)
+        {
+            culcData = CulcData.Instance;
+
+            if (Request.Form["result"] == "")
+            {
+                culcData.result = 0;
+            }
+            else culcData.result = Convert.ToInt32(Request.Form["result"]);
+
+            culcData.QuestionsData.Add($"{culcData.a} {culcData.operation} {culcData.b} = {culcData.result}");
+
+            if (culcData.result == culcData.Culc())
+            {
+                culcData.CountOfRightAnswers++;
+            }
+
+            if (Action == "Next")
+            {
+                CulcData culcData1 = CulcData.Instance;
+                culcData1.CreateQuestion();
+
+                return View(culcData1);
+            }
+            return RedirectToAction("Result");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab_2.3/Solution/Models/CulcData.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Solution.Models
+{
+    public class CulcData
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public string operation { get; set; }
+        public int result { get; set; }
+        public int CountOfQuestions { get; set; }
+        public int CountOfRightAnswers { get; set; }
+        public List<string> QuestionsData { get; set; }
+        public static CulcData Instance { get; set; } = new CulcData();
+
+        public CulcData()
+        {
+
+        }
+
+
+        public void Reset()
+        {
+            QuestionsData = new List<string>();
+            CountOfRightAnswers = 0;
+            CountOfQuestions = 0;
+        }
+
+        public void CreateQuestion()
+        {
+            CountOfQuestions++;
+            Random rnd = new Random();
+            int op = rnd.Next(4);
+            a = rnd.Next(10);
+            if (op == 3)
+            {
+                operation = "/";
+                b = rnd.Next(1, 10);
+            }
+            else
+            {
+                b = rnd.Next(10);
+                switch (op)
+                {
+                    case 0:
+                        operation = "+";
+                        break;
+                    case 1:
+                        operation = "-";
+                        break;
+                    case 2:
+                        operation = "*";
+                        break;
+                };
+
+            }
+        }
+
+        public int Culc()
+        {
+            return operation switch
+            {
+                "+" => a + b,
+                "-" => a - b,
+                "*" => a * b,
+                "/" => a / b,
+            };
+        }
+    }
+}
