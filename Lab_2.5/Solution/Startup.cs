e5c1ce82f6783e42a67e3ba23596d4eadab72ab2
Aleksey <aleksey_s@sibmail.com>
2022-05-02 14:48:05 +0700

    add Lab 5

---
+++ Lab_2.5/Solution/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Solution;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        string connection = Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<Solution.Models.DataContext>(options => options.UseSqlServer(connection));
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
