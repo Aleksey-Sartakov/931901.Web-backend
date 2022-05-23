336db097967a6133847e3cc08d521a4fa9ea6854
Aleksey <aleksey_s@sibmail.com>
2022-05-23 20:11:28 +0700

    add Lab 6

---
+++ Lab_2.6/Solution/Models/FolderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Solution.Models
+{
+    public class FolderViewModel
+    {
+        public string Name { get; set; }
+        public Guid? FolderId { get; set; }
+    }
+}
